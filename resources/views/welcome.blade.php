e7669c7fe3ab92d8bc2be8236ee4ee2f13815dfd
Yan Thiago <yan.basilio@hotmail.com>
2020-08-08 16:57:36 -0300

    bem vindo ao favors bruxo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Bem Vindo ao Favors Bruxo
                 </div>
 
                 <div class="links">
